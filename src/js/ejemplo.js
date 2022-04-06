f3f53435dd85449cbd70a7c2b87530276da870d0
JordanZ <78991970+JordanZR@users.noreply.github.com>
2022-04-06 12:40:30 -0600

    First commit

---
+++ src/js/ejemplo.js
@@ -0,0 +1,16 @@
+function createPDF(){
+    var doc = new jsPDF()
+    var email = document.getElementById("email").value
+    doc.text(email, 10, 10)
+    doc.save("YourPDF.pdf")
+}
+function dosPaginasPDF(){
+    var doc = new jsPDF()
+    var email = document.getElementById("email2").value
+    var password = document.getElementById("password").value
+    doc.text(20, 20, email)
+    doc.addPage()
+    doc.text(20, 20, password)
+    doc.save("YourPDF2.pdf")
+}
+
